7ff8a128d36e28a43afc57ec81bf72c17027e203
brynr garnett <brynrgarnett@gmail.com>
2026-05-07 22:12:22 -0500

    Initial Commit

---
+++ Guardian/GuardianApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuardianApp.swift
+//  Guardian
+//
+//  Created by Jonathan Garnett on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct GuardianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
