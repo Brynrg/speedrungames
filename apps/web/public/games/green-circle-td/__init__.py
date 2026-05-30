12f8995440c8351f5c2136aa64af96e49d92878c
brynr garnett <brynrgarnett@gmail.com>
2026-05-30 17:31:09 -0500

    Fix Green Circle TD structure - clean directory with minimal files

--- apps/web/public/games/green-circle-td/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Green Circle TD - Core game modules."""
-from .settings import *
-from .rng import Rng
-from .particle import Particle, ExplosionEffect
-from .enemy import Enemy
-from .tower import Tower
-from .bullet import Bullet, SplashBullet
-from .path import make_green_circle_path
-from .sim import Game
